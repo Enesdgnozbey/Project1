246010e15770c106d29c01dee4102f23f04b311f
Enes <Enes@DESKTOP-TP6U0TB>
2021-04-18 22:35:00 +0300

    Add project files.

---
+++ Project1/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace Project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(336, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "TIKLA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.BackgroundImage = global::Project1.Properties.Resources.eqweqw;
+            this.ClientSize = new System.Drawing.Size(591, 603);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Project1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //ctrl+shift+esc (Task Manager)
+         [DllImport("User32.dll")] static extern int SetForegroundWindow(IntPtr point);
+        private void button1_Click_1(object sender, EventArgs e)
+        {            
+
+            Process p = Process.GetProcessesByName("taskmanager").FirstOrDefault();
+            if (p != null)
+            {
+                IntPtr h = p.MainWindowHandle;
+                SetForegroundWindow(h);
+                //SendKeys.SendWait("");
+            }
+            else
+            {
+                Process islem = Process.Start("taskmgr.exe");
+                islem.WaitForInputIdle();
+                IntPtr h = islem.MainWindowHandle;
+                SetForegroundWindow(h);
+                SendKeys.SendWait("^(%({DEL}))");
+
+                MessageBox.Show("ctrl+alt+del not available");
+            }
+        }
+    }
+}
